c62e8c325fe7b4d19bbf14adbee0f81f5428f8e6
Olosko23 <oloogeorge633@gmail.com>
2023-11-03 14:45:06 +0300

    'permanent'

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import multer from "multer";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import path from "path";
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,21 +21,37 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.use("/uploads", express.static("public/files"));
+const permanentStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/file");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
-app.get("/", async (req, res) => {
-  try {
-    res.status(200).json("Up and Running");
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+const permanentUpload = multer({ storage: permanentStorage });
+
+app.use("/uploads", express.static("public/files"));
+app.use("/images", express.static("public/image"));
+
+app.get("/", (req, res) => {
+  res.status(200).json("Up and Running");
 });
 
 app.post("/upload", upload.single("file"), (req, res) => {
   if (req.file) {
-    res.status(200).json({
-      message: "File uploaded successfully",
-      originalname: req.file.originalname,
+    permanentUpload.single("file")(req, res, (err) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ message: "Failed to move file to permanent storage" });
+      } else {
+        res.status(200).json({
+          message: "File uploaded and moved to permanent storage successfully",
+          originalname: req.file.originalname,
+        });
+      }
     });
   } else {
     res.status(400).json({ message: "No file uploaded" });
@@ -43,8 +60,13 @@ app.post("/upload", upload.single("file"), (req, res) => {
 
 app.get("/download/:filename", (req, res) => {
   const filename = req.params.filename;
-  const file = path.join(__dirname, "public/files", filename);
-  res.download(file);
+  const filePath = path.join(__dirname, "public/files", filename);
+
+  if (fs.existsSync(filePath)) {
+    res.download(filePath);
+  } else {
+    res.status(404).json({ message: "File not found" });
+  }
 });
 
 const port = process.env.PORT || 3000;
